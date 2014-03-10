8978a5f65f5d1fc45be9b8bfc99685f519e0d365
Nigel Banks <nigel.g.banks@gmail.com>
2014-03-10 16:53:44 -0300

    Warnings who needs em.

--- templates/galleria-container.tpl.php
+++ templates/galleria-container.tpl.php
@@ -1,7 +1,9 @@
 <?php
 // Embed links.
 foreach ($items as $delta => &$item) {
-  $item['#item']['attributes'] = array('longdesc' => $item['#item']['image_field_caption']['value']);
+  if (isset($item['#item']['image_field_caption']['value'])) {
+    $item['#item']['attributes'] = array('longdesc' => $item['#item']['image_field_caption']['value']);
+  }
 }
 ?>
 <div class="galleria-content clearfix" id="galleria-<?php print $id; ?>">
