0109f82b60e70e01258b1f6bc459d23bc15f5de1
Phil Redmon <redmonp@missouri.edu>
2015-10-07 09:09:05 -0500

    Add missing quote

--- templates/page--islandora--object--islandora-collectionCModel.tpl.php
+++ templates/page--islandora--object--islandora-collectionCModel.tpl.php
@@ -91,7 +91,7 @@
       <div class="umkc-collection-header-description">
         <?php print html_entity_decode($islandora_object['description']['1']); ?>
       </div>
-      <div class="umkc-collection-header-thumbnail>
+      <div class="umkc-collection-header-thumbnail">
         <?php print render($islandora_object['thumbnail']); ?>
       </div>
     </div>
