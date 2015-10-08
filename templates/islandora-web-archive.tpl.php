b178196ffa9a6ae7f83120b05d31e2cfa51a8e6f
Phil Redmon <redmonp@missouri.edu>
2015-10-08 11:11:04 -0500

    Address mdpc:113. Override umkcdora collection template to remove parent collection code. This probably should be fixed in the umkcdora module at some point. Override all templates which show 'In collection'.

---
+++ templates/islandora-web-archive.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ * This is the template file for the object page for web archives
+ *
+ * @TODO: add documentation about file and available variables
+ */
+?>
+
+<div class="islandora-web-archive-object islandora">
+  <div class="islandora-web-archive-content-wrapper clearfix">
+    <?php if (isset($islandora_content)): ?>
+      <div class="islandora-web-archive-content">
+        <?php print $islandora_content; ?>
+      </div>
+    <?php endif; ?>
+    </div>
+    <div class="islandora-web-archive-metadata">
+      <?php print $description; ?>
+    <div>
+      <h2><?php print t('Download'); ?></h2>
+        <ul>
+          <?php if (isset($islandora_warc)): ?>
+            <li>Warc: <?php print $islandora_warc; ?>
+          <?php endif; ?>
+          <?php if (isset($islandora_pdf)): ?>
+            <li>PDF: <?php print $islandora_pdf; ?>
+          <?php endif; ?>
+          <?php if (isset($islandora_png)): ?>
+            <li>Screenshot: <?php print $islandora_png; ?>
+          <?php endif; ?>
+          <?php if (isset($islandora_csv)): ?>
+            <li>CSV: <?php print $islandora_csv; ?>
+          <?php endif; ?>
+        </ul>
+    </div>
+    <?php print $metadata; ?>
+  </div>
+</div>
---
+++ templates/umkcdora-collection-info.tpl.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @file
+ * UMKCdora collection info template.
+ */
+?>
