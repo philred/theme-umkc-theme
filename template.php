1f04287659ce411f819f50ab9641318cd12deca3
Phil Redmon <redmonp@missouri.edu>
2019-01-08 11:51:03 -0600

    Add new viewer params for ISLANDORA-2097

--- template.php
+++ template.php
@@ -237,6 +237,10 @@ function umkc_theme_preprocess_islandora_large_image(&$variables) {
         'query' => array('token' => $token),
       ));
     // Display large image.
+    $params['token'] = $token;
+    $params['pid'] = $islandora_object->id;
+    $params['dsid'] = 'JP2';
+    // Can be removed after 7.x-1.11 is out the door islandora_deprecated.
     $params['jp2_url'] = $jp2_url;
   }
 
